0afd91e72e21bde23ffd1c70ff0db0fde6130339
miyohide <miyohide@gmail.com>
2016-09-15 22:48:43 +0900

    create method for index action.

--- app/models/runlog.rb
+++ app/models/runlog.rb
@@ -1,2 +1,12 @@
 class Runlog < ActiveRecord::Base
+  def self.runlog_index
+    run_at_max = group(:run_count).maximum(:run_at)
+    run_at_min = group(:run_count).minimum(:run_at)
+
+    rval = {}
+    run_at_max.each do |key, val|
+      rval[key] = {max: val, min: run_at_min[key]}
+    end
+    rval
+  end
 end
