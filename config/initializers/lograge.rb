eb75295eeba5dc484461c8d196eb9ce061b5837b
miyohide <miyohide@gmail.com>
2016-10-14 23:28:21 +0900

    Add lograge setting.

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,17 @@
+Rails.application.configure do
+  path = File.join("#{Rails.root}", "log", "lograge_#{Rails.env}.log")
+  config.lograge.logger = ActiveSupport::Logger.new path
+  config.lograge.enabled = true
+  config.lograge.keep_original_rails_log = true
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
+
+  config.lograge.custom_options = lambda do |event|
+    exceptions = %w(controller action format id)
+    {
+      host: event.payload[:host],
+      time: event.time,
+      timespamp: event.time,
+      params: event.payload[:params].except(*exceptions)
+    }
+  end
+end
