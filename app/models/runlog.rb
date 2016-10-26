3d5c06308dd1a875fb89a3fd18105d615552ce9e
miyohide <miyohide@gmail.com>
2016-10-26 23:38:41 +0900

    delete time from polyline data.

--- app/models/runlog.rb
+++ app/models/runlog.rb
@@ -13,7 +13,7 @@ class Runlog < ActiveRecord::Base
   def self.to_polyline(run_count)
     polyline = []
     where(run_count: run_count).each do |runlog|
-      polyline << {lat: runlog.latitude.to_f, lng: runlog.longitude.to_f, time: Time.parse(runlog.dateandtime).to_i}
+      polyline << {lat: runlog.latitude.to_f, lng: runlog.longitude.to_f}
     end
     polyline
   end
